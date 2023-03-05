8f9a020940a457e00d6b81f04dc6f8d4fad09d61
Bloogefest <bloogefest@gmail.com>
2023-03-05 20:23:55 +0300

    Рестайлинг класса `PhotoSize`

--- bf_telegram/api/photo_size.py
+++ bf_telegram/api/photo_size.py
@@ -11,12 +11,7 @@ class PhotoSize:
     https://core.telegram.org/bots/api#photosize
     """
 
-    def __init__(self,
-                 file_id: str,
-                 file_unique_id: str,
-                 width: int,
-                 height: int,
-                 file_size: Optional[int] = None):
+    def __init__(self, file_id: str, file_unique_id: str, width: int, height: int, file_size: Optional[int] = None):
         """
         :param file_id: Identifier for this file, which can be used to download or reuse the file
         :param file_unique_id: Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
