d77b5f9c407f659733c23bec597f96668ad135bf
Bloogefest <bloogefest@gmail.com>
2023-03-05 17:14:57 +0300

    Исправление документации `Update`

--- bf_telegram/api/update.py
+++ bf_telegram/api/update.py
@@ -69,7 +69,7 @@ class Update:
                     timeout: Optional[int] = None,
                     allowed_updates: Optional[list[str]] = None) -> list[Update]:
         """
-        Use this method to receive incoming updates using long polling (wiki).
+        Use this method to receive incoming updates using long polling.
 
         :param offset: Identifier of the first update to be returned. Must be greater by one than the highest among the identifiers of previously received updates. By default, updates starting with the earliest unconfirmed update are returned. An update is considered confirmed as soon as getUpdates is called with an offset higher than its update_id. The negative offset can be specified to retrieve updates starting from -offset update from the end of the updates queue. All previous updates will forgotten.
         :param limit: Limits the number of updates to be retrieved. Values between 1-100 are accepted. Defaults to 100.
